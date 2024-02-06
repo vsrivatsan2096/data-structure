016d40bbf9392b5bc19c196e45f6c5f0815bcb71
vsrivatsan <vsrivatsan@vanenburg.com>
2024-02-06 19:28:38 +0530

    Implemented double linked list

--- src/main/java/data/structure/list/DoublyLinkedList.java
+++ src/main/java/data/structure/list/DoublyLinkedList.java
@@ -6,12 +6,76 @@ import data.structure.node.SinglyLinkedNode;
 public class DoublyLinkedList<T> extends LinkedList<T, DoublyLinkedNode<T>> {
     @Override
     public void insert(T data, int index) {
+        int size = getSize();
 
+        DoublyLinkedNode<T> node = new DoublyLinkedNode<>(data);
+
+        if (size == 0) {
+            setHead(node);
+            setTail(node);
+        } else if (index >= size) {
+            DoublyLinkedNode<T> temp = getTail();
+
+            temp.setNext(node);
+            node.setPrev(temp);
+
+            setTail(node);
+        } else {
+            DoublyLinkedNode<T> temp = getHead();
+
+            int i = 1;
+            while (i < index) {
+                temp = (DoublyLinkedNode<T>) temp.getNext();
+
+                i ++;
+            }
+
+            node.setNext(temp.getNext());
+            node.setPrev(temp);
+
+            ((DoublyLinkedNode<T>)temp.getNext()).setPrev(node);
+            temp.setNext(node);
+        }
+
+        incrementSize(1);
     }
 
     @Override
     public void delete(int index) {
+        int size = getSize();
 
+        if (size == 1) {
+            setHead(null);
+            setTail(null);
+        } else if (index >= size) {
+            setTail(getTail().getPrev());
+
+            getTail().setNext(null);
+        } else if (index == 0) {
+            setHead((DoublyLinkedNode<T>) getHead().getNext());
+
+            getHead().setPrev(null);
+        } else {
+            DoublyLinkedNode<T> temp = getHead();
+
+            int i = 1;
+            while (i < index) {
+                temp = (DoublyLinkedNode<T>) temp.getNext();
+
+                i ++;
+            }
+
+            if (temp.getNext() == getTail()) {
+                setTail(temp);
+            } else {
+                ((DoublyLinkedNode<T>)temp.getNext().getNext()).setPrev(temp);
+            }
+
+
+            temp.setNext(temp.getNext().getNext());
+        }
+
+        incrementSize(-1);
     }
 
     @Override
@@ -29,6 +93,7 @@ public class DoublyLinkedList<T> extends LinkedList<T, DoublyLinkedNode<T>> {
 
         String[] reverseStringArray = new String[getSize()];
 
+        i = 0;
         DoublyLinkedNode<T> doublyLinkedNode = getTail();
         while (doublyLinkedNode != null) {
             reverseStringArray[i ++] = doublyLinkedNode.getData().toString();
@@ -39,5 +104,23 @@ public class DoublyLinkedList<T> extends LinkedList<T, DoublyLinkedNode<T>> {
         return String.join(" -> ", forwardStringArray)
                 + "\n"
                 + String.join(" -> ", reverseStringArray);
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList<String> doublyLinkedList = new DoublyLinkedList<>();
+
+        doublyLinkedList.insert("First", 0);
+        doublyLinkedList.insert("Third", 1);
+        doublyLinkedList.insert("Second", 1);
+        doublyLinkedList.insert("Fourth", 3);
+        doublyLinkedList.insert("Fifth", 5);
+
+        System.out.println(doublyLinkedList);
+
+        doublyLinkedList.delete(0);
+        doublyLinkedList.delete(2);
+        doublyLinkedList.delete(2);
+
+        System.out.println(doublyLinkedList);
     }
 }
